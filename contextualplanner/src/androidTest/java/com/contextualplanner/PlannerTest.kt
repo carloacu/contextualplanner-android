d957682c0e8435232bcaf08bf1ace806f7d7210b
carloacu <carloacu.dev@gmail.com>
2022-05-24 10:39:01 +0200

    Add more tests

--- contextualplanner/src/androidTest/java/com/contextualplanner/PlannerTest.kt
+++ contextualplanner/src/androidTest/java/com/contextualplanner/PlannerTest.kt
@@ -2,24 +2,117 @@ package com.contextualplanner
 
 import org.junit.Test
 import org.junit.Assert.assertEquals
+import java.lang.Thread.sleep
 
 
 class PlannerTest {
 
-    private val greetActionId = "greet"
-    private val checkInActionId = "checkIn"
+    private val greetActionId = "greet-action"
+    private val checkInActionId = "checkIn-action"
+    private val informedAboutTheCompanyActionId = "informedAboutTheCompany-action"
+
+    private val greetedFact = "greeted"
+    private val checkedInFact = "checkedIn"
+    private val informedAboutTheCompanyFact = "informedAboutTheCompany"
 
     @Test
-    fun directLinkFromGoalTOEffect() {
+    fun directLinkFromGoalToEffect() {
         val actions = mutableListOf<Action>()
-        actions.add(Action(greetActionId, "", "", "greeted", "", arrayOf()))
-        actions.add(Action(checkInActionId, "", "", "checkedIn", "", arrayOf()))
+        actions.add(Action(greetActionId, "", "", greetedFact, "", arrayOf()))
+        actions.add(Action(checkInActionId, "", "", checkedInFact, "", arrayOf()))
         val domain = Domain(actions.toTypedArray())
         val problem = Problem()
-        problem.addGoals(arrayOf(Goal(10, "greeted")))
-        problem.addGoals(arrayOf(Goal(9, "checkedIn")))
+        problem.addGoals(arrayOf(Goal(10, checkedInFact)))
+        assertEquals(checkInActionId, lookForAnActionToDo(problem, domain))
+    }
+
+
+    @Test
+    fun preconditionResolution() {
+        val actions = mutableListOf<Action>()
+        actions.add(Action(greetActionId, "", "", greetedFact, "", arrayOf()))
+        actions.add(Action(checkInActionId, "", greetedFact, checkedInFact, "", arrayOf()))
+        val domain = Domain(actions.toTypedArray())
+        val problem = Problem()
+        problem.addGoals(arrayOf(Goal(10, checkedInFact)))
+        assertEquals(greetActionId, lookForAnActionToDo(problem, domain))
+        notifyActionDone(greetActionId, problem, domain)
+        assertEquals(checkInActionId, lookForAnActionToDo(problem, domain))
+    }
+
+    @Test
+    fun goalPriority() {
+        val actions = mutableListOf<Action>()
+        actions.add(Action(greetActionId, "", "", greetedFact, "", arrayOf()))
+        actions.add(Action(checkInActionId, "", "", checkedInFact, "", arrayOf()))
+        actions.add(Action(informedAboutTheCompanyActionId, "", "", informedAboutTheCompanyFact, "", arrayOf()))
+        val domain = Domain(actions.toTypedArray())
+        val problem = Problem()
+        problem.addGoals(arrayOf(Goal(9, checkedInFact)))
+        problem.addGoals(arrayOf(Goal(10, greetedFact)))
+        problem.addGoals(arrayOf(Goal(9, informedAboutTheCompanyFact)))
 
         assertEquals(greetActionId, lookForAnActionToDo(problem, domain))
     }
 
+
+    @Test
+    fun goalStackable() {
+        val actions = mutableListOf<Action>()
+        actions.add(Action(greetActionId, "", "", greetedFact, "", arrayOf()))
+        actions.add(Action(checkInActionId, "", "", checkedInFact, "", arrayOf()))
+        val domain = Domain(actions.toTypedArray())
+        val problem = Problem()
+        problem.addGoals(arrayOf(Goal(9, checkedInFact, stackable = true)))
+        problem.addGoals(arrayOf(Goal(10, greetedFact)))
+        assertEquals(greetActionId, lookForAnActionToDo(problem, domain))
+        notifyActionDone(greetActionId, problem, domain)
+        assertEquals(checkInActionId, lookForAnActionToDo(problem, domain))
+    }
+
+
+    @Test
+    fun goalNotStackable() {
+        val actions = mutableListOf<Action>()
+        actions.add(Action(greetActionId, "", "", greetedFact, "", arrayOf()))
+        actions.add(Action(checkInActionId, "", "", checkedInFact, "", arrayOf()))
+        val domain = Domain(actions.toTypedArray())
+        val problem = Problem()
+        problem.addGoals(arrayOf(Goal(9, checkedInFact, stackable = false)))
+        problem.addGoals(arrayOf(Goal(10, greetedFact)))
+        assertEquals(greetActionId, lookForAnActionToDo(problem, domain))
+        notifyActionDone(greetActionId, problem, domain)
+        assertEquals("", lookForAnActionToDo(problem, domain))
+    }
+
+
+    @Test
+    fun goalInactiveLessThanTheMaxTimeToKeepInactive() {
+        val actions = mutableListOf<Action>()
+        actions.add(Action(greetActionId, "", "", greetedFact, "", arrayOf()))
+        actions.add(Action(checkInActionId, "", "", checkedInFact, "", arrayOf()))
+        val domain = Domain(actions.toTypedArray())
+        val problem = Problem()
+        problem.addGoals(arrayOf(Goal(9, checkedInFact, true, maxTimeToKeepInactive = 10)))
+        problem.addGoals(arrayOf(Goal(10, greetedFact)))
+        assertEquals(greetActionId, lookForAnActionToDo(problem, domain))
+        notifyActionDone(greetActionId, problem, domain)
+        assertEquals(checkInActionId, lookForAnActionToDo(problem, domain))
+    }
+
+
+    @Test
+    fun goalInactiveMoreThanTheMaxTimeToKeepInactive() {
+        val actions = mutableListOf<Action>()
+        actions.add(Action(greetActionId, "", "", greetedFact, "", arrayOf()))
+        actions.add(Action(checkInActionId, "", "", checkedInFact, "", arrayOf()))
+        val domain = Domain(actions.toTypedArray())
+        val problem = Problem()
+        problem.addGoals(arrayOf(Goal(9, checkedInFact, true, maxTimeToKeepInactive = 1)))
+        problem.addGoals(arrayOf(Goal(10, greetedFact)))
+        assertEquals(greetActionId, lookForAnActionToDo(problem, domain))
+        notifyActionDone(greetActionId, problem, domain)
+        sleep(2000)
+        assertEquals("", lookForAnActionToDo(problem, domain))
+    }
 }
